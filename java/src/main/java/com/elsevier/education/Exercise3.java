7c9b8c81be5e0118c42c3f0649f3aee6501bec04
VijayaRemala <Vijaya.Remala@cognizant.com>
2016-11-01 15:50:30 +0530

    Update Exercise3.java
    VijayRemala: Vijaya.Remala@cognizant.com

--- java/src/main/java/com/elsevier/education/Exercise3.java
+++ java/src/main/java/com/elsevier/education/Exercise3.java
@@ -19,7 +19,10 @@ public class Exercise3 {
 		people.add(p1);
 		people.add(p1);
 		
+		/* we can get only one object here */
 		System.out.println(people.size());
+		
+	/** in this if we remove the hashcode and eqals methods we can get the 2 objects*/
 		
 	}
 	
@@ -32,10 +35,14 @@ public class Exercise3 {
 			id = newId;
 		}
 		
+		/** by using this hashcode method we can check the instance of the obects*/
+		@Override
 		public int hashCode() {
 			return id * generator.nextInt();
 		}
 		
+		/** by using this equals method we can check the content of the objects*/
+		@Override
 		public boolean equals(Object other) {
 			return id.equals(((Person)other).id);
 		}
