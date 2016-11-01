6117510121ceb6e46cd20fcfc9ce288bb9ad8dac
VijayaRemala <Vijaya.Remala@cognizant.com>
2016-11-01 15:55:27 +0530

    Update Exercise4.java

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -5,13 +5,15 @@ package com.elsevier.education;
 TODO Is Counter thread-safe? If so, why, and if not, how can we fix it?
 
 */
+
+/** we need do syncronization in method level */
 public class Exercise4 {
 
 	public static class Counter {
 		
 		private int count = 0;
 		
-		public int increment() {
+		public syncronized int increment() {
 			return ++count;
 		}
 		
