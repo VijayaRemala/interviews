b7d58e2e853deb11683a43d77c0047f4b60fb3c1
VijayaRemala <Vijaya.Remala@cognizant.com>
2016-11-03 10:50:13 +0530

    Vijay Remala:vijaya.Remala@conizant.com

--- java/src/main/java/com/elsevier/education/Exercise1.java
+++ java/src/main/java/com/elsevier/education/Exercise1.java
@@ -5,36 +5,42 @@ package com.elsevier.education;
 TODO: Make this class immutable.
 
 */
-public class Exercise1 {
+// final class we can not extends the scope
+public final class Exercise1 {
 
 	public static class Person {
-		
-		private Set<String> phoneNumbers;
-		private String firstName;
-		private String lastName;
+		//because of final we can assain the values only once
+		private final Set<String> phoneNumbers;
+		private final String firstName;
+		private final String lastName;
 		
 		public Person() {
 		}
+    
+             /**
 
+	  One Argumented constructor is required to set the values for fields
+         */
+		
 		public Set<String> getPhoneNumbers() {
 			return phoneNumbers;
 		}
-		public void setPhoneNumbers(Set<String> newPhoneNumbers) {
-			phoneNumbers = newPhoneNumbers;
-		}
+		//public void setPhoneNumbers(Set<String> newPhoneNumbers) {
+		//	phoneNumbers = newPhoneNumbers;
+		//}
 		
 		public String getFirstName() {
 			return firstName;
 		}
-		public void setFirstName(String newName) {
-			firstName = newName;
-		}
+		//public void setFirstName(String newName) {
+		//	firstName = newName;
+		//}
 		
 		public String getLastName() {
 			return lastName;
 		}
-		public void setLastName(String newName) {
-			lastName = newName;
-		}
+		//public void setLastName(String newName) {
+		//	lastName = newName;
+		//}
 	}
 }
